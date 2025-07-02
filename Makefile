c147ee27540a9c4d3ebcce95b5659cae8819aff6
benshi <807629978@qq.com>
2025-07-02 08:58:25 +0800

    feat(docker): add MySQL installation and configuration for memory storage
    * Install MySQL server in the Docker image.
    * Configure MySQL to use memory storage with specific settings.
    * Update entrypoint script to set MySQL root password and flush privileges.

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 TARGET = x86_64-unknown-linux-musl
 
+.PHONY: build
+
 build:
 	cross build --release --target $(TARGET)
 	cp target/$(TARGET)/release/lsp-ws-proxy build/
@@ -12,3 +14,6 @@ run:
 
 image:
 	cd ./build && docker build -t lsp-ws-proxy:latest -f Dockerfile .
+
+push:
+	docker push lsp-ws-proxy:latest
--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+mysqld_safe --defaults-file=/etc/mysql/conf.d/memory.cnf &
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+
 exec /work/lsp-ws-proxy --listen 9999 --remap -c /etc/lsp-ws-proxy/config.json
